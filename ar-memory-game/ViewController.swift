10ac375f51a72a32e98d2c9a3b8e4c320c2a99ba
Sebastiaan Hols <s.hols@icloud.com>
2020-04-06 22:42:15 +0200

    Added images to scene and added onTap recogniser

--- ar-memory-game/ViewController.swift
+++ ar-memory-game/ViewController.swift
@@ -13,13 +13,73 @@ class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        let tapLocation = sender.location(in: arView)
+        // Get the entity at the location we've tapped, if one exists
+        if let card = arView.entity(at: tapLocation) {
+            // For testing purposes, print the name of the tapped entity
+            print("Card hit! \(card.name)")
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        // Create an anchor plane for the game
+        let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.2, 0.2])
+        arView.scene.addAnchor(anchor)
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        // Setup tap recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action:#selector(onTap))
+        arView.addGestureRecognizer(tapGesture)
+        
+        // Create card templates
+        var cardTemplates: [Entity] = []
+        
+        for index in 1...8 {
+            let assetName = "memory_card_\(index)"
+            let cardTemplate = try! Entity.loadModel(named: assetName)
+            cardTemplate.generateCollisionShapes(recursive: true)
+            cardTemplate.name = assetName
+            cardTemplates.append(cardTemplate)
+        }
+        
+        // Clone and create the cards
+        var cards: [Entity] = []
+        
+        for cardTemplate in cardTemplates {
+            for _ in 1...2 {
+                cards.append(cardTemplate.clone(recursive: true))
+            }
+        }
+        
+        cards.shuffle()
+        
+        // Place the cards
+        
+        for (index, card) in cards.enumerated(){
+            
+            let x = Float(index % 4) - 1.5
+            let z = Float(index / 4) - 1.5
+            
+            card.position = [x * 0.1, 0, z * 0.1]
+            card.setScale(SIMD3(repeating: Float(0.0015)), relativeTo: anchor)
+            
+            
+            // Todo: Setup flip animation 12:51
+            
+            // Setup animation
+            var flipDownTransform = card.transform
+            flipDownTransform.rotation = simd_quatf(angle: .pi, axis: [1,0,0])
+            
+            let flipDownController = card.move(to: flipDownTransform, relativeTo: card.parent, duration: 0.25, timingFunction: .easeOut)
+            
+//           flipDownController.
+            
+            anchor.addChild(card)
+            
+        }
+        
     }
 }
